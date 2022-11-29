d46152c4e3756e76ade3fa9c47fc96b8b3905d35
ANSHUMAN PRATYUSH <apratyush777@gmail.com>
2022-11-30 00:24:43 +0530

    adding JS assignment 1

---
+++ counter.js
@@ -0,0 +1,10 @@
+let count=0;
+document.getElementById("counting").innerText = count;
+function increment(){
+    count=count+1;
+    document.getElementById("counting").innerText = count;
+}
+function decrement(){
+    count=count-1
+    document.getElementById("counting").innerText = count;
+}
